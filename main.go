635ee1b2998978ae0385caac43de2d371857a183
Oana-Andreea Ilie <andreeaa.ilie516@gmail.com>
2023-12-05 23:53:58 +0200

    Fetch by ID added

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -37,10 +38,56 @@ func getEventModels(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, eventModels)
 }
 
+func eventById(id string) (*event, error) {
+	for i, b := range events {
+		if b.ID == id {
+			return &events[i], nil
+		}
+	}
+
+	return nil, errors.New("event not found")
+}
+
+func getEventById(c *gin.Context) {
+	id := c.Param("id")
+	event, err := eventById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "event not found!"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, event)
+}
+
+func eventModelById(id string) (*eventModel, error) {
+	for i, b := range eventModels {
+		if b.ID == id {
+			return &eventModels[i], nil
+		}
+	}
+
+	return nil, errors.New("event model not found")
+}
+
+func getEventModelById(c *gin.Context) {
+	id := c.Param("id")
+	eventModel, err := eventModelById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "event model not found!"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, eventModel)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/getEvents", getEvents)
 	router.GET("/getEventModels", getEventModels)
+	router.GET("/getEventById/:id", getEventById)
+	router.GET("/getEventModelById/:id", getEventModelById)
 	err := router.Run("localhost:8080")
 	if err != nil {
 		return
