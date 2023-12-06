346a085e6905df2a8f79e1ee9e46958447cd9988
Oana-Andreea Ilie <andreeaa.ilie516@gmail.com>
2023-12-06 20:23:30 +0200

    Revert "User CRUD operations added"
    This reverts commit a8ff3f567d22e6018a6cc1eea2de2da336c12af1.

--- domain/entities/city.go
+++
@@ -1,8 +0,0 @@
-package entities
-
-type City struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Country     string `json:"country"`
-	Description string `json:"description"`
-}
